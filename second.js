4d621adcc2706c8615d4399d7878aa998c08265d
Amna Farooq <97411510+Amna445@users.noreply.github.com>
2024-04-24 08:04:23 +0000

    commit

---
+++ second.js
@@ -0,0 +1,4 @@
+// There are two main types of data types,
+// 1. Premitive,  2. non_premitive
+//  There are seven categories of premitive data types: 
+//  Types: string, Number, Bolean, null (empty), undefined, symbol(unique),Bigint
