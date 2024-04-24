58feb78cb5fd0bfe18b95ff0cd61aabe70911aa6
Amna Farooq <97411510+Amna445@users.noreply.github.com>
2024-04-24 11:16:51 +0000

    comit

--- second.js
+++ second.js
@@ -2,3 +2,8 @@
 // 1. Premitive,  2. non_premitive
 //  There are seven categories of premitive data types: 
 //  Types: string, Number, Bolean, null (empty), undefined, symbol(unique),Bigint
+  
+
+// Non-Premitive  (Reference type)
+//  array, objects, functions,
+
